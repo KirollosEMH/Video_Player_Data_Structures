0d7355954215e9fccab2855c54ea8758ed4708dc
Ahmed-Nezar <21P0025@eng.asu.edu.eg>
2023-11-19 22:03:08 +0200

    update: commented include of opencv

--- VideoPlayer.cpp
+++ VideoPlayer.cpp
@@ -1,10 +1,9 @@
 #include "CircularLinkedList.hpp"
-#include <opencv2/opencv.hpp>
+//#include <opencv2/opencv.hpp>
 #include <iostream>
 using namespace std;
 
-
 int main(){
-    
+    cout << "Hello World" << endl;    
     return 0;
 }
