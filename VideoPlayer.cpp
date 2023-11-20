a0cea10967e9eedb72bee17c9405647f399b63c9
Ahmed-Nezar <21P0025@eng.asu.edu.eg>
2023-11-21 00:41:06 +0200

    add: more test cases in main function

--- VideoPlayer.cpp
+++ VideoPlayer.cpp
@@ -29,5 +29,14 @@ int main(){
     cout << "List2: "<<list2;
     list1.erase(6);
     cout << "List1: "<< list1;
+    cout << "List1 value 4 at location: "<< list1.search(4)<<endl;
+    cout << "List1 value 5 at location: "<< list1.search(5)<<endl;
+    cout << "List1 value 6 at location: "<< list1.search(6)<<endl;
+    list2.erase(0);
+    list2.erase(0);
+    list2.erase(0);
+    list2.erase(0);
+    cout << "List2: "<<list2; 
+    cout << "List2 value 4 at location: "<< list2.search(4)<<endl;
     return 0;
 }
