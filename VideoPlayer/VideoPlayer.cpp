7ec7f8f58d9251f44fbc3a10f605cacd7ba60260
Ahmed-Nezar <ahmednezarrr@gmail.com>
2023-12-19 16:00:02 +0200

    fix: add function now takes string as whole without quotations

--- VideoPlayer/VideoPlayer.cpp
+++ VideoPlayer/VideoPlayer.cpp
@@ -223,8 +223,8 @@ void VideoPlayer::addVideoRuntime() {
         string videoPath;
         cout << "Enter the path of the video to add: ";
         getline(cin, videoPath);
-        cout << videoPath;
-        addVideo(videoPath.substr(1, videoPath.length() - 2));
+        //cout << videoPath;
+        addVideo(videoPath);
     } else {
         cerr << "No playlist selected. Please select a playlist first." << endl;
     }
